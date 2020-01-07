7f1ad39a6840969d756e03ac59099aa8eec33ed0
Giunio DE LUCA <giuniodl@live.it>
2020-01-07 11:58:12 +0100

    ajout méthode ajouterEtudiantFormation

--- src/main/java/fr/intiformation/dao/EtudiantDAO.java
+++ src/main/java/fr/intiformation/dao/EtudiantDAO.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.intiformation.domain.Etudiant;
+import fr.intiformation.domain.Formation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Transactional
 @Repository
-public interface EtudiantDAO extends JpaRepository<Etudiant, Long>{
+public interface EtudiantDAO extends JpaRepository<Etudiant, Long> {
+
+	@Modifying
+	@Query("update Etudiant e set e.formation.idFormation=:idFormation where e.idEtudiant = :idEtudiant ")
+	public void ajouterEtudiantFormation(@Param("idEtudiant") Long idEtudiant, @Param("idFormation") Long idFormation);
 }
